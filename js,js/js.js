4a49d6724b852ce6230de62b510d6459b29d9947
G91688 <gultacmansimova0493@gmail.com>
2022-10-21 22:38:22 +0200

    praktika1

---
+++ js,js/js.js
@@ -0,0 +1,20 @@
+lert("default")
+document.write("js dersi <br>")
+document.write("js dersi2 <br>")
+alert("gultac")
+
+prompt("enter your username")
+confirm ("Abune olmaq isdeyirsinizmi")
+console.log("men burdayam")
+document.getElementById("demo2").innerHTML = "esmira muellime"
+
+
+function adi_goster(){
+    document.getElementById("demo2").innerHTML = "esmira muellime"
+
+    document.getElementsByClassName("demo")[0].innerHTML = "nese"
+
+
+}
+
+document.getElementsByTagName("div")[8].innerHTML = "hello Gultac"
